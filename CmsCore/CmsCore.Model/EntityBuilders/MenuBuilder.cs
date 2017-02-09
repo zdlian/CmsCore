2f050ffa7f75a7a7acde143b3bce1e3f3fc83e6b
mdemirci01 <mdemirci@outlook.com>
2017-02-09 19:21:14 +0300

    removed whitespace

--- CmsCore/CmsCore.Model/EntityBuilders/MenuBuilder.cs
+++ CmsCore/CmsCore.Model/EntityBuilders/MenuBuilder.cs
@@ -15,10 +15,6 @@ namespace CmsCore.Model.EntityBuilders
             entityBuilder.Property(e => e.Name).IsRequired().HasMaxLength(200);
             entityBuilder.HasOne(e => e.MenuLocation).WithOne(l => l.Menu).HasForeignKey<MenuLocation>(m => m.MenuId).HasForeignKey<Menu>(m=>m.MenuLocationId);
             entityBuilder.HasMany(I => I.MenuItems).WithOne(m => m.Menu).HasForeignKey(s => s.MenuId);
-
-
-
-
         }
     }
 }
