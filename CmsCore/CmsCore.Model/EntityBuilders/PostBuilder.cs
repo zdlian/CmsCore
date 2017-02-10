38587ab265ed29bbc2496bfc6316c414e0bd87f7
AliZalim <ali.zalim@hotmail.com>
2017-02-10 14:58:47 +0300

    template files added

---
+++ CmsCore/CmsCore.Model/EntityBuilders/PostBuilder.cs
@@ -0,0 +1,28 @@
+﻿using CmsCore.Model.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCore.Model.EntityBuilders
+{
+    public class PostBuilder
+    {
+
+        public PostBuilder(EntityTypeBuilder<Post>entityBuilder)
+        {
+            entityBuilder.HasKey(p => p.Id);
+            entityBuilder.Property(p => p.Title).IsRequired().HasMaxLength(200);
+            entityBuilder.Property(p => p.Slug).IsRequired().HasMaxLength(200);
+            entityBuilder.Property(p => p.Body).IsRequired().HasMaxLength(250);
+            entityBuilder.Property(p => p.SeoTitle).HasMaxLength(200);
+            entityBuilder.Property(p => p.SeoDescription);
+            entityBuilder.Property(p => p.SeoKeywords);
+            entityBuilder.Property(p => p.IsPublished).IsRequired();
+
+            
+        }
+
+    }
+}
---
+++ CmsCore/CmsCore.Model/EntityBuilders/SettingBuilder.cs
@@ -0,0 +1,19 @@
+﻿using CmsCore.Model.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCore.Model.EntityBuilders
+{
+    public class SettingBuilder
+    {
+        public SettingBuilder(EntityTypeBuilder<Setting> entityBuilder)
+        {
+            entityBuilder.HasKey(s => s.Id);
+            entityBuilder.Property(s => s.Name).IsRequired().HasMaxLength(200);
+            entityBuilder.Property(s => s.Value).IsRequired().HasMaxLength(200);
+        }
+    }
+}
