f3954e3052e5b140392c7446eeb301a0a11876fa
CEVDET29 <cevdetbas29@gmail.com>
2017-02-11 16:28:03 +0200

    menu create

--- CmsCore/CmsCore.Admin/Controllers/MenuController.cs
+++ CmsCore/CmsCore.Admin/Controllers/MenuController.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CmsCore.Service;
 using CmsCore.Model.Entities;
+using CmsCore.Admin.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CmsCore.Admin.Controllers
 {
-    public class MenuController : Controller
+    public class MenuController : BaseController
     {
         private readonly IMenuService menuService;
         public MenuController(IMenuService menuService) {
@@ -55,6 +56,31 @@ namespace CmsCore.Admin.Controllers
             menuService.DeleteMenu(id);
             menuService.SaveMenu();
             return RedirectToAction("Index", "Menu");
+        }
+        public ActionResult AjaxHandler(jQueryDataTableParamModel param)
+        {
+            string sSearch = "";
+            if (param.sSearch != null) sSearch = param.sSearch;
+            var sortColumnIndex = Convert.ToInt32(Request.Query["iSortCol_0"]);
+            var sortDirection = Request.Query["sSortDir_0"]; // asc or desc
+            int iTotalRecords;
+            int iTotalDisplayRecords;
+            var displayedPages = menuService.Search(sSearch, sortColumnIndex, sortDirection, param.iDisplayStart, param.iDisplayLength, out iTotalRecords, out iTotalDisplayRecords);
+
+            var result = from p in displayedPages
+                         select new[] {
+                             p.Id.ToString(),
+                             p.Name.ToString(),
+                             p.MenuLocation.Name.ToString()
+                             ,string.Empty
+                         };
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = iTotalRecords,
+                iTotalDisplayRecords = iTotalDisplayRecords,
+                aaData = result.ToList()
+            });
         }
     }
 }
